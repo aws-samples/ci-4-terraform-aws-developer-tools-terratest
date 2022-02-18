aab02fa4afce83c13d17f1036c1fe6bab9b749da
Michael Costas <micostas@amazon.com>
2022-02-18 18:50:28 +0000

    refactor for s3 tf4

--- terraform/modules/cicd/s3.tf
+++ terraform/modules/cicd/s3.tf
@@ -1,36 +1,49 @@
 resource "aws_s3_bucket" "codebuild_bucket" {
   bucket = "${var.pipeline_deployment_bucket_name}-codebuild"
+  tags = var.custom_tags
+}
+
+resource "aws_s3_bucket_acl" "codebuild_bucket_acl" {
+  bucket = aws_s3_bucket.codebuild_bucket.id
   acl    = "private"
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
-      }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "enforce_sse_kms_encryption_codebuild_bucket" {
+  bucket = aws_s3_bucket.codebuild_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
     }
   }
-
-  tags = var.custom_tags
 }
 
 resource "aws_s3_bucket" "codepipeline_bucket" {
   bucket = "${var.pipeline_deployment_bucket_name}-codepipeline"
-  acl    = "private"
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
-      }
-    }
-  }
-
-  # Neede for CloudWatch
-  versioning {
-    enabled = true
-  }
-
   tags = var.custom_tags
 }
+
+resource "aws_s3_bucket_versioning" "versioning_codepipeline" {
+  # Need for CloudWatch
+  bucket = aws_s3_bucket.codepipeline_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_acl" "codepipeline_bucket_acl" {
+  bucket = aws_s3_bucket.codepipeline_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "enforce_sse_kms_encryption_codepipeline_bucket" {
+  bucket = aws_s3_bucket.codepipeline_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
 
 resource "aws_s3_bucket_public_access_block" "pipeline_buckets" {
 
